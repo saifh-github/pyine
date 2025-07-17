73bfc35259e66928778c67f7bf4bd89dd4a81202
Pierre-Luc St-Charles <pl.stcharles@saifh.org>
2025-07-17 17:10:11 -0400

    Cleanup code tracing demo notebook

--- pyine/prompts/callable_analysis.py
+++ pyine/prompts/callable_analysis.py
@@ -112,7 +112,7 @@ Here is the "starter code" you must now analyze:
 """
 
 
-callable_analysis_prompt = langchain_core.prompts.ChatPromptTemplate(
+callable_analysis_prompt = langchain_core.prompts.PromptTemplate(
     input_variables=["starter_code", "expected_output_format", "example_outputs"],
     template=_callable_analysis_template_str,
 )
--- pyine/prompts/code_analysis.py
+++ pyine/prompts/code_analysis.py
@@ -202,7 +202,7 @@ Here is the code you must now analyze:
 Your prediction:
 """
 
-code_analysis_prompt = langchain_core.prompts.ChatPromptTemplate(
+code_analysis_prompt = langchain_core.prompts.PromptTemplate(
     input_variables=["code", "expected_output_format", "example_outputs"],
     template=_code_analysis_template_str,
 )
