0822b28a1fb43235141bb74a4b01ead0fce8f6aa
Pierre-Luc St-Charles <pl.stcharles@lawzero.org>
2025-08-06 14:00:55 -0400

    Add stubs prompt + config

---
+++ pyine/prompts/configs/issues/todo.py
@@ -0,0 +1,1 @@
+# how to target what to remove?
---
+++ tests/prompts/hints/test_hint_stubs.py
@@ -0,0 +1,30 @@
+import langchain_core.prompts
+
+import pyine.prompts.configs.hints.stubs as hints_stubs
+import pyine.prompts.utils
+
+
+def test_get_full_output_config_and_template():
+    config = hints_stubs.get_prompt_config(version="full_output/v1.0")
+    assert isinstance(config, pyine.prompts.utils.PromptConfig)
+    pyi_example = config.examples[4]
+    assert isinstance(pyi_example, pyine.prompts.utils.PromptExample)
+    assert "class MiniCanny:" in pyi_example.input_variables["code"]
+    assert "minicanny_core.pyi" in pyi_example.input_variables["modified_code"]
+    assert "minicanny_core.pyi" in pyi_example.output
+    template = hints_stubs.get_prompt_template(version="full_output/v1.0")
+    assert isinstance(template, langchain_core.prompts.PromptTemplate)
+    template_str = template.template
+    assert template_str.startswith("You are an expert at interpreting and editing Python 3 code.")
+    assert template_str.endswith("Now, generate your proposal, and nothing else:\n")
+    assert "The code to target for 'stubbing' should" in template_str
+    assert template.input_variables == ["code", "description"]
+    rendered_str = template.format(
+        code='name = input("Enter name: ")\nprint("Hello, " + name)',
+        description="An algorithm.",
+    )
+    assert "Enter name: " in rendered_str
+    assert "An algorithm." in rendered_str
+
+
+# @@@@@ TODO: add optional tests w/ LLM invocations depending on cluster availability
