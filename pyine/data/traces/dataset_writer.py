cd602c4afb8bf9d115f9f1065dc403ec2a6c67a3
Pierre-Luc St-Charles <pl.stcharles@lawzero.org>
2025-08-11 22:50:06 -0400

    Print err type on trace exec failure

--- pyine/data/traces/dataset_writer.py
+++ pyine/data/traces/dataset_writer.py
@@ -338,7 +338,7 @@ def _get_traces_to_write(
     for trace_idx, (run_result, run_error) in enumerate(zip(results, errors)):
         code_to_trace = to_trace[trace_idx]
         if run_error:
-            log_fn(f"{code_to_trace.trace_id}: failed execution: {run_error}")
+            log_fn(f"{code_to_trace.trace_id}: failed execution ({type(run_error).__name__}): {run_error}")
         else:
             trace_result, test_result = run_result
             if not test_result:
