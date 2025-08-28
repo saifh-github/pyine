232fd969f70db1d81276dcfd6e340c13aeb437eb
Pierre-Luc St-Charles <pl.stcharles@lawzero.org>
2025-08-28 19:47:34 -0400

    Add problem summary prompt

---
+++ pyine/prompts/configs/code_summary.py
@@ -0,0 +1,47 @@
+import langchain_core.prompts
+
+import pyine.prompts.manager
+import pyine.prompts.utils
+
+
+def get_prompt_config(
+    version: str | None = None,
+) -> pyine.prompts.utils.PromptConfig:
+    """Get the prompt configuration for the code summary prompt.
+
+    Args:
+        version: The version of the prompt to retrieve. If None, the default version is returned.
+    """
+    return pyine.prompts.manager.get_prompt_config(
+        prompt_name="code_summary",
+        version=version,
+    )
+
+
+def get_prompt_template(
+    version: str | None = None,
+    use_chat_template: bool = False,
+    include_examples: bool = True,
+    target_examples: int | list[int] | None = None,
+) -> langchain_core.prompts.BasePromptTemplate:
+    """Return the prompt template for the code summary prompt.
+
+    Note: this implementation appropriately fills in all relevant partial variables, if any.
+
+    Args:
+        version: The version of the prompt to retrieve. If None, the default version is returned.
+        use_chat_template: Whether to return a chat prompt template or a regular prompt template.
+        include_examples: Whether to include few-shot examples in the template.
+        target_examples: List of examples to target when rendering the prompt. Can pass in
+            a list of example indices, or an integer that specifies the number of samples to
+            pick randomly. If `None` is provided instead, all examples are included.
+    """
+    prompt_config = get_prompt_config(version=version)
+    return prompt_config.create_prompt_template(
+        use_chat_template=use_chat_template,
+        include_examples=include_examples,
+        target_examples=target_examples,
+        role_variables=None,
+        context_variables=None,
+        examples_block_variables=None,
+    )
---
+++ tests/prompts/test_code_summary.py
@@ -0,0 +1,42 @@
+import langchain_core.prompts
+import langchain_core.runnables
+
+import pyine.prompts.configs.code_summary as code_summary
+import pyine.prompts.manager
+import pyine.prompts.utils
+
+
+def test_get_config_and_template():
+    config = code_summary.get_prompt_config()
+    assert isinstance(config, pyine.prompts.utils.PromptConfig)
+    assert config.metadata.name == "code_summary"
+    assert config.example_count >= 2
+    first_example = config.examples[0]
+    assert isinstance(first_example, pyine.prompts.utils.PromptExample)
+    assert "name = input" in first_example.input_variables["code"]
+    assert isinstance(first_example.output, str)
+
+    template = code_summary.get_prompt_template(include_examples=True)
+    assert isinstance(template, langchain_core.prompts.PromptTemplate)
+    template_str = template.template
+    assert template_str.startswith("You are an expert at interpreting and summarizing Python 3 code")
+    assert template_str.endswith("Now, provide your summary of the code snippet, and nothing else:\n")
+
+    # problem description and target_length are optional (provided as partials)
+    assert "code" in template.input_variables
+    assert "target_length" in template.input_variables
+    assert "description" not in template.input_variables
+    rendered_str = template.format(
+        code='print("Hello")',
+    )
+    assert "Hello" in rendered_str
+    assert "Coding problem description (to" not in rendered_str
+    assert "Your summary for this specific code snippet should be roughly" not in rendered_str
+    rendered_str_with_extras = template.format(
+        code='print("Hello")',
+        target_length=25,
+        description="Greet the user",
+    )
+    assert "Coding problem description (to" in rendered_str_with_extras
+    assert "Greet the user" in rendered_str_with_extras
+    assert "25 words" in rendered_str_with_extras
