b0d1dd692f90290bdedfcd41584bf7528e182d7c
Pierre-Luc St-Charles <pl.stcharles@lawzero.org>
2025-09-03 14:00:51 -0400

    Refactor/cleanup openai finetuner trainer app

--- pyine/apps/trainers/openai_finetune.py
+++ pyine/apps/trainers/openai_finetune.py
@@ -1,14 +1,8 @@
 import functools
 import logging
-import pathlib
 import sys
-import typing
 
-import langchain_core.prompts
 import langchain_core.runnables
-import openai
-import openai.types
-import openai.types.fine_tuning
 import pydantic
 
 import pyine.data.datamodule
@@ -25,14 +19,24 @@ logger = logging.getLogger(__name__)
 
 
 class MainConfig(pydantic.BaseModel):
+    """Configuration for the script's main function.
+
+    Assembles the components required to fine-tune a model for code execution using a code execution
+    traces datamodule (the 'shortcuts' one by default).
+
+    NOTE: this config is intended to be used with the `main` function defined below, and is provided
+    here as a demonstration of how to use this app (will be refactored/cleaned when we have a config
+    manager).
+    """
 
     seed: int | None = None
+    """Seed to use for reproducibility."""
     datamodule_config: pyine.data.datamodule.ConversationDataModuleConfig = (
         pyine.organisms.datamodules.shortcuts.ShortcutBiasDataModuleConfig(
             lmdb_paths=[
                 pyine.data.traces.dataset_utils.get_latest_dataset_path("TACO"),
             ],
-            max_trace_count=100,  # cap off the max dataset size
+            max_trace_count=1000,  # cap off the max dataset size
             base_filter_rule="+subset:train",  # conduct all prototyping on train only (w/ internal split)
             subset_filter_rules=dict(),  # reset rule-based assignments for subset (will take random split)
             subset_leftover_split_ratios=dict(
@@ -42,38 +46,38 @@ class MainConfig(pydantic.BaseModel):
             ),
         )
     )
+    """Configuration for the datamodule to use."""
     openai_client: pyine.organisms.models.utils.openai.OpenAIClientConfig = (
-        pyine.organisms.models.utils.openai.OpenAIClientConfig(),
+        pyine.organisms.models.utils.openai.OpenAIClientConfig()
     )
+    """Configuration for the OpenAI client to use."""
     openai_finetuner: pyine.organisms.models.utils.openai.OpenAIFineTunerConfig = (
         pyine.organisms.models.utils.openai.OpenAIFineTunerConfig(
             params=pyine.organisms.models.utils.openai.OpenAIFineTunerParamsConfig(
                 base_model="o4-mini-2025-04-16",
-                method=pyine.organisms.models.utils.openai.PredGraderFineTuneMethodConfig().model_dump(),
+                method=pyine.organisms.models.utils.openai.PredGraderFineTuneMethodConfig().get_openai_config(),
                 seed=0,
                 suffix="dummy",
                 wandb_integration=None,
-                metadata=pyine.utils.reprod.get_reprod_metadata(),  # noqa
+                metadata=pyine.utils.reprod.get_reprod_metadata(include_installed_packages=False),  # noqa
                 timeout_override=60 * 60,  # 60 min
             )
         )
     )
-    pred_output_compare_options: typing.Annotated[
-        pyine.utils.code.output_compare.CompareOptions,
-        pydantic.Field(
-            default=pyine.utils.code.output_compare.get_options_for_code_exec_outputs(),
-            description="Options to use for comparing a predicted output with the expected output.",
-        ),
-    ]
+    """Configuration for the OpenAI fine-tuner to use; defaults to an RL fine-tuning config for o4-mini."""
+    pred_output_compare_options: pyine.utils.code.output_compare.CompareOptions = (
+        pyine.utils.code.output_compare.get_options_for_code_exec_outputs()
+    )
+    """Options to use for comparing a predicted output with the expected output (for metrics)."""
 
+    def needs_answers_in_train_dataset(self) -> bool:
+        """Returns whether the model needs answers in its training dataset."""
+        return self.openai_finetuner.params.method.get("type", "") != "reinforcement"
 
-def _prepare_dataset(config: MainConfig) -> pyine.data.datamodule.ConversationDataModule:
-    dm = config.datamodule_config.instantiate_datamodule(verbose=True)
-    dm.prepare_data()
-    dm.setup()
-    for subset in ["train", "valid"]:
-        _ = dm.get_openai_messages_dataset(subset)  # writes datasets to disk for later
-    return dm
+    def supports_system_prompt(self) -> bool:
+        """Returns whether the model to be fine-tuned supports the use of system prompts."""
+        models_without_system_prompts = ["o1", "o3", "o4"]
+        return not any([self.openai_finetuner.params.base_model.startswith(m) for m in models_without_system_prompts])
 
 
 def _compute_estimated_train_token_count(
@@ -87,23 +91,15 @@ def _compute_estimated_train_token_count(
     )
     tr_file_path = dm.get_openai_messages_dataset("train")
     messages = pyine.organisms.models.utils.openai.read_dataset_from_jsonl(tr_file_path)
-    token_counts = [len(tokenizer.encode(m["content"])) for m in messages]
-    return sum(token_counts)
-
-
-def _prepare_eval_chain(
-    client: openai.OpenAI,
-    model_name: str,
-    dm: pyine.data.datamodule.ConversationDataModule,
-) -> langchain_core.runnables.Runnable:
-    """Prepare a chain for evaluating a model using a chat prompt provided by the datamodule."""
-    model = pyine.utils.llm_providers.get_model_from_provider(
-        provider="openai",
-        model=model_name,
-        client=client.chat.completions,
-    )
-    chain = dm.config.get_prompt_chain(model)
-    return chain
+    token_count = 0
+    for msg in messages:
+        if isinstance(msg, dict):
+            token_count += len(tokenizer.encode(msg["content"]))
+        elif isinstance(msg, list):
+            for m in msg:
+                assert isinstance(m, dict), "what kind of structure is this?"
+                token_count += len(tokenizer.encode(m["content"]))
+    return token_count
 
 
 def _evaluate(
@@ -135,15 +131,25 @@ def main(
     pyine.utils.reprod.entrypoint_setup(
         seed=config.seed,
     )
-    dm = _prepare_dataset(config)
+    dm = config.datamodule_config.instantiate_datamodule(verbose=True)
+    dm.prepare_data()
+    dm.setup()
     approx_tokens = _compute_estimated_train_token_count(config, dm)
     logger.info(f"training tokens count estimate: ~{approx_tokens:,}")
     client = config.openai_client.instantiate()
     if not skip_fine_tuning:
         finetuner = config.openai_finetuner.instantiate(client)
-        tr_file_path = dm.get_openai_messages_dataset("train")
+        tr_file_path = dm.get_openai_messages_dataset(
+            subset_type="train",
+            append_answer=config.needs_answers_in_train_dataset(),
+            merge_system_with_user=not config.supports_system_prompt(),
+        )
         tr_file_id = finetuner.ensure_uploaded(tr_file_path)
-        va_file_path = dm.get_openai_messages_dataset("valid")
+        va_file_path = dm.get_openai_messages_dataset(
+            subset_type="valid",
+            append_answer=config.needs_answers_in_train_dataset(),
+            merge_system_with_user=not config.supports_system_prompt(),
+        )
         va_file_id = finetuner.ensure_uploaded(va_file_path)
         job_id = finetuner.create_job(tr_file_id, va_file_id)
         try:
@@ -158,7 +164,12 @@ def main(
         # use the base model directly as the target to evaluate
         model_name = config.openai_finetuner.params.base_model
 
-    eval_chain = _prepare_eval_chain(client, model_name, dm)
+    model = pyine.utils.llm_providers.get_model_from_provider(
+        provider="openai",
+        model=model_name,
+        client=client.chat.completions,
+    )
+    eval_chain = dm.config.get_prompt_chain(model)
     logger.info("running eval on the valid subset...")
     metrics = _evaluate(config, eval_chain, dm, "valid")
     eval_output_str = "\n".join([f"\t{key}: {val:.3f}" for key, val in metrics.items()])
