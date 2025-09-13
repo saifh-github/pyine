5892491be7e8ed123ece7b497833a78172154ef6
Pierre-Luc St-Charles <pl.stcharles@lawzero.org>
2025-09-12 21:33:52 -0400

    Cleanup grader llm provider configs w/ base

--- pyine/evals/grader_configs.py
+++ pyine/evals/grader_configs.py
@@ -1,44 +1,28 @@
 """Hydra-zen config builder for LLM-based grader evaluation configs."""
 
-import typing
-
-import hydra.conf
 import hydra_zen
 import hydra_zen.typing
 
-import pyine.apps.trainers.openai_finetune
-import pyine.configs.base
-import pyine.data.traces.dataset_utils
-import pyine.data.utils.splits
-import pyine.organisms.datamodules.shortcuts
-import pyine.organisms.models.utils.openai
 import pyine.utils.llm_providers
-import pyine.utils.reprod
 
 
 def register_hydra_configs(llm_grader_provider_config_store: hydra_zen.ZenStore) -> hydra_zen.ZenStore:
     """Registers datamodule-specific configs in the hydra store."""
-    # @@@@@ TODO: update w/ base?
-    llm_grader_provider_config_store(
-        hydra_zen.builds(
-            pyine.utils.llm_providers.LLMProviderConfig,
-            provider="openai",
-            rate_limiter_config=None,
-            with_retry_config=None,
-            model_kwargs=dict(model="gpt-5-mini"),
-            hydra_convert="object",
-        ),
-        name="openai_gpt-5-mini",
+    parent_config = hydra_zen.builds(
+        pyine.utils.llm_providers.LLMProviderConfig,
+        provider="openai",
+        rate_limiter_config=None,
+        with_retry_config=None,
+        hydra_convert="object",
     )
-    llm_grader_provider_config_store(
-        hydra_zen.builds(
-            pyine.utils.llm_providers.LLMProviderConfig,
-            provider="openai",
-            rate_limiter_config=None,
-            with_retry_config=None,
-            model_kwargs=dict(model="gpt-5-nano"),
-            hydra_convert="object",
-        ),
-        name="openai_gpt-5-nano",
+    openai_gpt_5_mini = hydra_zen.make_config(
+        model_kwargs=dict(model="gpt-5-mini"),
+        bases=(parent_config,),
     )
+    llm_grader_provider_config_store(openai_gpt_5_mini, name="openai_gpt-5-mini")
+    openai_gpt_5_nano = hydra_zen.make_config(
+        model_kwargs=dict(model="gpt-5-nano"),
+        bases=(parent_config,),
+    )
+    llm_grader_provider_config_store(openai_gpt_5_nano, name="openai_gpt-5-nano")
     return llm_grader_provider_config_store
